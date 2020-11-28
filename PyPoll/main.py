f85683a4963cceeab6a1b5bf84806058c7dfe29f
Ismahan Adan <ismahanadan@ismahans-mbp.home>
2020-11-28 18:04:09 -0500

    my commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,19 +13,27 @@ with open(election_csv) as csv_file:
     csv_header = next(csv_file)
     #print(f"Header: {csv_header}")
     row_count = 0
-    votes =[]
+    votes =[] #row0
     candidates = [] 
     candidate_votes = []
-    # Read through each row of data after the header
+    # Read through each row of data after the header to get total vote count
     for row in csv_reader:
         row_count += 1
-        candidate_name = row[2]
+        votes = row[0]
+        candidate_name = row[2] #row2
+        # Get list of candidate name
         for candidate in candidate_name:
             if candidate_name not in candidates:
                 candidates.append(candidate_name)
+        # Get count of votes each candidate received   
         for candidate in candidate_name:
-            candidate_votes.append(candidate_name.count(candidate))
-            
+            candidate_votes.append(candidate_name.count(candidate))     
+
+        
+        
+
+
+
 
 
 
@@ -33,14 +41,14 @@ with open(election_csv) as csv_file:
             
         
        
-        #print(len(csv_reader))
+       
 
     print("Election Results")
     print("-----------------------------")     
     print(f'Total Votes: {row_count}')
     print("------------------------------")
     print(candidates) 
-    #print(f"[candidates[i]}: {'{: .2%}' .format(row_count[i]/len(candidate_name))} ({row_count[i]})")
+    print(candidate_votes)
 
      
     #print("Winner: Khan")
