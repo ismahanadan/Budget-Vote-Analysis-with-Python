8dfd050065306da409354353ebf85bbc4088f92c
Ismahan Adan <ismahanadan@ismahans-mbp.home>
2020-11-28 00:48:59 -0500

    my commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,24 +11,32 @@ with open(election_csv) as csv_file:
     votes = {'Voter ID': 'row[0]' , 'County': 'row[1]' , 'Candidate': 'row[2]'}
     # Read the header row first 
     csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+    #print(f"Header: {csv_header}")
     row_count = 0
-    candidate = str('row2')
-
+    #Candidate = string
+    candidates = [] 
     # Read through each row of data after the header
     for row in csv_reader:
         row_count += 1
+        candidate_name = row[2]
+        for candidate in candidate_name:
+            if candidate_name not in candidates:
+                candidates.append(candidate_name)
+            
+        
        
         #print(len(csv_reader))
          
     print(f'Number of votes: {row_count}')
-#List of Candidates
-        candidate.sort({row[2]})
-    print(row[2])
+    print(candidates) 
+    
+     
+    
 
-
+        
 
     
+
 
 
 
