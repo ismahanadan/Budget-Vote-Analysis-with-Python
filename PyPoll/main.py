90fd4d15ff394555984b542a3d4ea49d850eb758
Ismahan Adan <ismahanadan@ismahans-mbp.home>
2020-11-29 17:23:46 -0500

    my commits

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,8 +3,9 @@ import csv
 
 election_csv = '/Users/ismahanadan/Desktop/python-challenge/PyPoll/election_data.csv'
 
-candidates = [] 
-candidate_votes = []
+votes = []
+candidates = []
+
 # Open and read csv
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
@@ -13,20 +14,18 @@ with open(election_csv) as csv_file:
     csv_header = next(csv_file)
     #print(f"Header: {csv_header}")
     row_count = 0
-    
+    votes = []
     # Read through each row of data after the header to get total vote count
     for row in csv_reader:
         row_count += 1
-        candidate_votes.append(row[0])
-       # Get list of candidate name
-
-        for candidate in candidates:
-            if candidate not in candidates:
-                candidate.append(row[2])
-        # Get count of votes each candidate received   
-        #candidate_votes[candidates] = 0
-        #candidate_votes.append(candidates.count(candidates))  
-        #candidate_votes[candidates] = candidate_votes[candidates] + 1   
+        votes = row[0]
+        candidates = row[2]
+        candidate_name = []
+    #Sort individual candidates in the list of candidates
+    for candidate_name  in candidates:
+        if candidate_name not in candidates:
+            candidates.append(candidate_name)
+       
 
         
         
@@ -40,8 +39,8 @@ with open(election_csv) as csv_file:
     print("-----------------------------")     
     print(f'Total Votes: {row_count}')
     print("------------------------------")
-    #print(f"candidate") 
-    #print(f"")
+    print(f"candidate_names") 
+    #print(f'Winner: {candidates[candidate_votes.index(max(candidate_votes))]}')
 
 #To export a text file with the results
 with  open("ByPoll_output.txt", "w") as text_file:
