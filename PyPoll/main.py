24671fe3058aa7193cdb493126d57ebd4fd06226
Ismahan Adan <ismahanadan@ismahans-mbp.home>
2020-11-29 04:27:46 -0500

    my commits

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,32 @@
 import os
 import csv
 
-#election_csv = os.path.join('election_data.csv')
-
 election_csv = '/Users/ismahanadan/Desktop/python-challenge/PyPoll/election_data.csv'
 
+candidates = [] 
+candidate_votes = []
 # Open and read csv
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-    votes = {'Voter ID': 'row[0]' , 'County': 'row[1]' , 'Candidate': 'row[2]'}
+    
     # Read the header row first 
     csv_header = next(csv_file)
     #print(f"Header: {csv_header}")
     row_count = 0
-    votes =[] #row0
-    candidates = [] 
-    candidate_votes = []
+    
     # Read through each row of data after the header to get total vote count
     for row in csv_reader:
         row_count += 1
-        votes = row[0]
-        candidate_name = row[2] #row2
-        # Get list of candidate name
-        #for candidate in candidate_name:
-        if candidate_name not in candidates:
-            candidates.append(candidate_name)
+        candidate_votes.append(row[0])
+       # Get list of candidate name
+
+        for candidate in candidates:
+            if candidate not in candidates:
+                candidate.append(row[2])
         # Get count of votes each candidate received   
-        candidate_votes[candidate_name] = 0
-            #candidate_votes.append(candidate_name.count(candidate))  
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1   
+        #candidate_votes[candidates] = 0
+        #candidate_votes.append(candidates.count(candidates))  
+        #candidate_votes[candidates] = candidate_votes[candidates] + 1   
 
         
         
@@ -42,8 +40,8 @@ with open(election_csv) as csv_file:
     print("-----------------------------")     
     print(f'Total Votes: {row_count}')
     print("------------------------------")
-    print(candidates) 
-    print(f"")
+    #print(f"candidate") 
+    #print(f"")
 
    
 
